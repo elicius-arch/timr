0393295000644a44fe67136fb705d1fffb373757
STUCKYM <wellingtonzealand@gmail.com>
2020-09-30 13:00:34 +0200

    App-Icon geändert

--- app/src/main/java/com/elicius/timr/TimerActivity.java
+++ app/src/main/java/com/elicius/timr/TimerActivity.java
@@ -169,10 +169,10 @@ public class TimerActivity extends AppCompatActivity {
         Vibrator v = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
             // Vibrate for 500 milliseconds
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+            v.vibrate(VibrationEffect.createOneShot(10, VibrationEffect.EFFECT_CLICK));
         } else {
             //deprecated in API 26
-            v.vibrate(500);
+            v.vibrate(10);
         }
     }
 
